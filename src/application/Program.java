02e62543c13da2dc0bbad8c874c92fdbb6b6b33e
MiltonRafael <miltonrafael166@gmail.com>
2026-01-07 13:50:27 -0300

    first class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hellow world");
+		Position pos = new Position(1, 2);
+		System.out.println(pos);
 	}
 
 }
