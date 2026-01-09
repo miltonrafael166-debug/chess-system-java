6be17857384bcd1f3fa10ccf9d52ad8f2d741611
MiltonRafael <miltonrafael166@gmail.com>
2026-01-09 09:27:37 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,8 +19,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
-				System.out.println();
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(scan);
@@ -29,7 +28,6 @@ public class Program {
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
-				System.out.println();
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(scan);
--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -42,6 +43,14 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("error reading chessPosition: valid values are from a1 to h8");
 		}
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println();
+		System.out.println("turn: " + chessMatch.getTurn());
+		System.out.println("waiting players: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
