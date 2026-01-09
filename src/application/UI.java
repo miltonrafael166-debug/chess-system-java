1861111a5f6260338e902d665390774a126e124b
MiltonRafael <miltonrafael166@gmail.com>
2026-01-09 13:15:50 -0300

    Check mate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,11 +31,11 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	public static void clearScreen() {
-		 System.out.print("\033[H\033[2J");
-		 System.out.flush();
-		}
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
 	public static ChessPosition readChessPosition(Scanner scan) {
 		try {
@@ -47,17 +47,21 @@ public class UI {
 			throw new InputMismatchException("error reading chessPosition: valid values are from a1 to h8");
 		}
 	}
-	
+
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("turn: " + chessMatch.getTurn());
-		System.out.println("waiting players: " + chessMatch.getCurrentPlayer());
-		
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK !");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("waiting players: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK !");
+			}
+		} else {
+			System.out.println("CHECK MATE !");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
@@ -71,7 +75,7 @@ public class UI {
 		}
 		System.out.print("  a b c d e f g h ");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(8 - i + " ");
@@ -98,10 +102,12 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 		System.out.println("captured pieces:");
 		System.out.print("white: ");
 		System.out.print(ANSI_WHITE);
